d955f89767dc5cc8d63d36b1c6fdb4a2e00cb69a
Eastar-DS <dororo0104@naver.com>
2020-11-03 17:09:07 +0900

    20201103

--- Digit_Recognizer/digit_recognizer.py
+++ Digit_Recognizer/digit_recognizer.py
@@ -6,7 +6,16 @@ Machine learning with Digit
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
 import seaborn as sns
+'matplotlib inline'
+
+'난수 일정하게 만들기'
+np.random.seed(2)
+
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix
+import itertools
 
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.tree import DecisionTreeClassifier
@@ -14,6 +23,15 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.naive_bayes import GaussianNB
 from sklearn.svm import SVC
 
+'''
+# convert to one-hot-encoding
+from keras.utils.np_utils import to_categorical 
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten, Conv2D, MaxPool2D
+from keras.optimizers import RMSprop
+from keras.preprocessing.image import ImageDataGenerator
+from keras.callbacks import ReduceLROnPlateau
+'''
 
 train = pd.read_csv("train.csv")
 test = pd.read_csv("test.csv")
@@ -21,15 +39,31 @@ test = pd.read_csv("test.csv")
 Y_train = train["label"]
 X_train = train.drop(labels = ["label"],axis = 1)
 
+del train
+
+g = sns.countplot(Y_train)
 
 
+'2.2 Check for null and missing values'
+
+X_train.isnull().any().describe()
+test.isnull().any().describe()
 
 
+'2.3 Normalization'
+X_train = X_train / 255.0
+test = test / 255.0
+
+'2.3 Reshape'
+'데이터를 [0]의 형태(,,,1)로 28x28짜리 행렬로 만들어서 데이터개수(-1,)만큼 펼침.'
+X_train = X_train.values.reshape(-1,28,28,1)
+test = test.values.reshape(-1,28,28,1)
+
+'2.5 Label encoding'
+'Y_train = to_categorical(Y_train, num_classes = 10)'
 
 
-
-
-
+'import tensorflow'
 
 
 
