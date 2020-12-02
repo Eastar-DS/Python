b9213ae6c9e668c7ac18317d7ab2360a47d52e79
Eastar-DS <dororo0104@naver.com>
2020-12-02 18:02:26 +0900

    2020_12_02

--- Leetcode/Easy_Problems.py
+++ Leetcode/Easy_Problems.py
@@ -2,13 +2,23 @@
 #     def twoSum(self, nums: List[int], target: int) -> List[int]:
 
 
-class Solution:
-    def twoSum(self, nums: [int], target: int) -> list[int]:
+# class Solution:
+    # def twoSum(self, nums: [int], target: int) -> list[int]:
+    #     for i in range(len(nums)-1):
+    #         start = i
+    #         for j in range(len(nums)-(i+1)):
+    #             Sum = nums[i] + nums[i+j+1]
+    #             if((target - Sum) == 0):
+    #                 Output = [i,i+j+1]
+    #                 return Output
+    
+
+
+
+def twoSum(nums, target):
         for i in range(len(nums)-1):
-            start = i
             for j in range(len(nums)-(i+1)):
                 Sum = nums[i] + nums[i+j+1]
                 if((target - Sum) == 0):
                     Output = [i,i+j+1]
                     return Output
-    
