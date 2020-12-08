442e27f380323c23f42242123e973c4c3658cef8
Eastar-DS <dororo0104@naver.com>
2020-12-08 16:19:35 +0900

    I was sooooo busyyyyy TT

--- Leetcode/Easy_Problems.py
+++ Leetcode/Easy_Problems.py
@@ -3,22 +3,37 @@
 
 
 # class Solution:
-    # def twoSum(self, nums: [int], target: int) -> list[int]:
-    #     for i in range(len(nums)-1):
-    #         start = i
-    #         for j in range(len(nums)-(i+1)):
-    #             Sum = nums[i] + nums[i+j+1]
-    #             if((target - Sum) == 0):
-    #                 Output = [i,i+j+1]
-    #                 return Output
+#     def twoSum(self, nums: [int], target: int) -> list[int]:
+#         for i in range(len(nums)-1):
+#             start = i
+#             for j in range(len(nums)-(i+1)):
+#                 Sum = nums[i] + nums[i+j+1]
+#                 if((target - Sum) == 0):
+#                     Output = [i,i+j+1]
+#                     return Output
     
 
 
 
-def twoSum(nums, target):
+# def twoSum(nums, target):
+#         for i in range(len(nums)-1):
+#             for j in range(len(nums)-(i+1)):
+#                 Sum = nums[i] + nums[i+j+1]
+#                 if((target - Sum) == 0):
+#                     Output = [i,i+j+1]
+#                     return Output
+                
+                
+class Solution(object):
+    def twoSum(nums, target):
         for i in range(len(nums)-1):
             for j in range(len(nums)-(i+1)):
                 Sum = nums[i] + nums[i+j+1]
                 if((target - Sum) == 0):
                     Output = [i,i+j+1]
                     return Output
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
