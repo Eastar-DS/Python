4a00e187f44356485f7fac346385c34fe53fc144
Eastar-DS <dororo0104@naver.com>
2021-03-17 17:00:28 +0900

    2021_0317

---
+++ Kaggle/Indoor_Location_and_Navigation/practice.py
@@ -0,0 +1,145 @@
+#1
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+
+from dataclasses import dataclass
+
+import matplotlib.pyplot as plt # visualization
+plt.rcParams.update({'font.size': 14})
+import seaborn as sns # visualization
+
+import warnings # Supress warnings 
+warnings.filterwarnings('ignore')
+
+from tqdm import tqdm
+
+import json
+'import plotly.graph_objs as go'
+from PIL import Image
+
+#2
+class ReadData:
+    acce: np.ndarray
+    acce_uncali: np.ndarray
+    gyro: np.ndarray
+    gyro_uncali: np.ndarray
+    magn: np.ndarray
+    magn_uncali: np.ndarray
+    ahrs: np.ndarray
+    wifi: np.ndarray
+    ibeacon: np.ndarray
+    waypoint: np.ndarray
+
+
+def read_data_file(data_filename):
+    acce = []
+    acce_uncali = []
+    gyro = []
+    gyro_uncali = []
+    magn = []
+    magn_uncali = []
+    ahrs = []
+    wifi = []
+    ibeacon = []
+    waypoint = []
+
+    with open(data_filename, 'r', encoding='utf-8') as file:
+        lines = file.readlines()
+
+    for line_data in lines:
+        line_data = line_data.strip()
+        if not line_data or line_data[0] == '#':
+            continue
+
+        line_data = line_data.split('\t')
+
+        if line_data[1] == 'TYPE_WAYPOINT':
+            waypoint.append([int(line_data[0]), float(line_data[2]), float(line_data[3])])
+            continue
+       
+        if line_data[1] == 'TYPE_ACCELEROMETER':
+            acce.append([int(line_data[0]), float(line_data[2]), float(line_data[3]), float(line_data[4])])
+            continue
+        
+        if line_data[1] == 'TYPE_ACCELEROMETER_UNCALIBRATED':
+            acce_uncali.append([int(line_data[0]), float(line_data[2]), float(line_data[3]), float(line_data[4])])
+            continue
+        
+        if line_data[1] == 'TYPE_GYROSCOPE':
+            gyro.append([int(line_data[0]), float(line_data[2]), float(line_data[3]), float(line_data[4])])
+            continue
+
+        if line_data[1] == 'TYPE_GYROSCOPE_UNCALIBRATED':
+            gyro_uncali.append([int(line_data[0]), float(line_data[2]), float(line_data[3]), float(line_data[4])])
+            continue
+        
+        if line_data[1] == 'TYPE_MAGNETIC_FIELD':
+            magn.append([int(line_data[0]), float(line_data[2]), float(line_data[3]), float(line_data[4])])
+            continue
+
+        if line_data[1] == 'TYPE_MAGNETIC_FIELD_UNCALIBRATED':
+            magn_uncali.append([int(line_data[0]), float(line_data[2]), float(line_data[3]), float(line_data[4])])
+            continue
+
+        if line_data[1] == 'TYPE_ROTATION_VECTOR':
+            ahrs.append([int(line_data[0]), float(line_data[2]), float(line_data[3]), float(line_data[4])])
+            continue
+
+        if line_data[1] == 'TYPE_WIFI':
+            sys_ts = line_data[0]
+            ssid = line_data[2]
+            bssid = line_data[3]
+            rssi = line_data[4]
+            lastseen_ts = line_data[6]
+            wifi_data = [sys_ts, ssid, bssid, rssi, lastseen_ts]
+            wifi.append(wifi_data)
+            continue
+
+        if line_data[1] == 'TYPE_BEACON':
+            ts = line_data[0]
+            uuid = line_data[2]
+            major = line_data[3]
+            minor = line_data[4]
+            rssi = line_data[6]
+            ibeacon_data = [ts, '_'.join([uuid, major, minor]), rssi]
+            ibeacon.append(ibeacon_data)
+            continue
+        
+    
+    acce = np.array(acce)
+    acce_uncali = np.array(acce_uncali)
+    gyro = np.array(gyro)
+    gyro_uncali = np.array(gyro_uncali)
+    magn = np.array(magn)
+    magn_uncali = np.array(magn_uncali)
+    ahrs = np.array(ahrs)
+    wifi = np.array(wifi)
+    ibeacon = np.array(ibeacon)
+    waypoint = np.array(waypoint)
+    
+    return ReadData(acce, acce_uncali, gyro, gyro_uncali, magn, magn_uncali, ahrs, wifi, ibeacon, waypoint)
+
+sample_file = read_data_file("../input/indoor-location-navigation/train/5a0546857ecc773753327266/F2/5dccf516c04f060006e6e3c9.txt")
+
+print('acce shape:', sample_file.acce.shape)
+print('acce_uncali shape:', sample_file.acce_uncali.shape)
+print('gyro shape:', sample_file.gyro.shape)
+print('gyro_uncali shape:', sample_file.gyro_uncali.shape)
+print('magn shape:', sample_file.magn.shape)
+print('magn_uncali shape:',sample_file.magn_uncali.shape)
+print('ahrs shape:', sample_file.ahrs.shape)
+print('wifi shape:', sample_file.wifi.shape)
+print('ibeacon shape:', sample_file.ibeacon.shape)
+print('waypoint shape:', sample_file.waypoint.shape)
+
+
+
+
+
+
+
+
+
+
+
+
