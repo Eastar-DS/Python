bc0315bb9f4fdbb89ab53f521f5a5e7ab336b948
Eastar-DS <dororo0104@naver.com>
2021-04-08 06:52:26 +0900

    Hello man

--- Interview/chap7_Array.py
+++ Interview/chap7_Array.py
@@ -168,7 +168,7 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        #파이썬다운풀이 wow ...!
+        #파이썬다운풀이 wow ...!hihihihihihi
         return sum(sorted(nums)[::2])
     
     
