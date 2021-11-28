1fbbb8434149546d51ce97408d5780614df276f8
Eastar-DS <dororo0104@naver.com>
2021-11-28 20:23:47 +0900

    2021_1128

--- 14 Days Study Plan about Data_Structure/Data_Structure1.py
+++ 14 Days Study Plan about Data_Structure/Data_Structure1.py
@@ -14,6 +14,16 @@ Day 2 Array : 1. Two Sum, 88. Merge Sorted Array
 Day 3 Array : 350. Intersection of Two Arrays II, 121. Best Time to Buy and Sell Stock
 Day 4 Array : 566. Reshape the Matrix, 118. Pascal's Triangle
 Day 5 Array : 36. Valid Sudoku, 74. Search a 2D Matrix
+Day 6 String : 
+    387. First Unique Character in a String, 383. Ransom Note, 242. Valid Anagram
+Day 7 Linked List : 
+    141. Linked List Cycle, 21. Merge Two Sorted Lists, 203. Remove Linked List Elements
+Day 8 Linked List : 
+    206. Reverse Linked List, 83. Remove Duplicates from Sorted List
+Day 9 Stack / Queue : 
+    20. Valid Parentheses, 232. Implement Queue using Stacks
+
+
 """
 
 class Solution:
@@ -85,18 +95,96 @@ class Solution:
         # return profit
     
     
+#day4
+    def matrixReshape(self, mat: List[List[int]], r: int, c: int) -> List[List[int]]:
+        import numpy as np
+        try:
+            return np.reshape(mat, (r, c)).tolist()
+        except:
+            return mat
+        
+
+    # def matrixReshape2(self, mat: List[List[int]], r: int, c: int) -> List[List[int]]:
+    #     flat = sum(nums, [])
+    #     if len(flat) != r * c:
+    #         return nums
+    #     tuples = zip(*([iter(flat)] * c))
+    #     return map(list, tuples)
+
+        
+    #     return nums if len(sum(nums, [])) != r * c else map(list, zip(*([iter(sum(nums, []))]*c)))
+
+    
+    # def matrixReshape3(self, mat: List[List[int]], r: int, c: int) -> List[List[int]]:
+    #     if r * c != len(nums) * len(nums[0]):
+    #         return nums
+    #     it = itertools.chain(*nums)
+    #     return [list(itertools.islice(it, c)) for _ in xrange(r)]
     
     
+    def generate(self, numRows: int) -> List[List[int]]:
+        res = [[1]]
+        for i in range(1, numRows):
+            res += [map(lambda x, y: x+y, res[-1] + [0], [0] + res[-1])]
+        return res[:numRows]
+  
+    
+    def generate2(numRows):
+        pascal = [[1]*(i+1) for i in range(numRows)]
+        for i in range(numRows):
+            for j in range(1,i):
+                pascal[i][j] = pascal[i-1][j-1] + pascal[i-1][j]
+        return pascal
     
     
-    
-    
-    
-    
-    
-    
-    
-    
+#day5
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        """
+        Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 3
+        Output: true
+        
+        Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 13
+        Output: false
+        """
+        if(target < matrix[0][0] or matrix[-1][-1] < target):
+            return False
+        
+        l,r = 0, len(matrix)
+        row = (l+r)//2
+        while(l + 1 != r):
+            if(target >= matrix[row][0]):
+                l = row
+            else:
+                r = row
+            row = (l+r)//2
+            
+        if(target in matrix[row]):
+            return True
+        
+        return False
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     
     
     
@@ -268,6 +356,122 @@ Output: 23
             return profit
         else:
             return 0
+
+    
+#day4
+    def matrixReshape(self, mat: List[List[int]], r: int, c: int) -> List[List[int]]:
+        """
+        Input: mat = [[1,2],[3,4]], r = 1, c = 4
+        Output: [[1,2,3,4]]
+        
+        Input: mat = [[1,2],[3,4]], r = 2, c = 4
+        Output: [[1,2],[3,4]]
+        """
+
+        m , n = len(mat), len(mat[0])
+        if(m * n != r * c):
+            return mat
+        
+        temp_mat = []
+        output = []
+        #temp_mat : [1,2,3,4,...] 처럼 만들기
+        for array in mat:
+            temp_mat += array 
+        
+        temp_array = []
+        for i in range(m*n):
+            if(i%c == c-1):
+                temp_array.append(temp_mat[i])
+                output.append(temp_array)
+                temp_array = []
+            else:
+                temp_array.append(temp_mat[i])
+        
+        return output
+            
+
+
+    def generate(self, numRows: int) -> List[List[int]]:
+        """
+        Input: numRows = 5
+        Output: [[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]]
+        """
+        if(numRows == 1):
+            return [[1]]
+        if(numRows == 2):
+            return[[1],[1,1]]
+        
+        output = [[1],[1,1]]
+        def Pascal(post_pascal:List[int]) -> List[int]:
+            next_pascal = [1]
+            for i in range(len(post_pascal) - 1):
+                next_pascal.append(post_pascal[i] + post_pascal[i+1])
+            next_pascal.append(1)
+            return next_pascal              
+                        
+        for i in range(numRows - 2):
+            output.append(Pascal(output[-1]))
+            
+        return output
+        
+
+#day5
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        #1 row
+        for row in board:
+            temp = []
+            for element in row:
+                if(element != "."):
+                    temp.append(element)
+            if(len(temp) != len(set(temp))):
+                return False        
+            
+        #2 col
+        for i in range(9):
+            temp = [row[i] for row in board if(row[i] != ".")]
+            
+            if(len(temp) != len(set(temp))):
+                return False
+        #3 square
+        for i in range(3):
+            for j in range(3):
+                temp = [board[3*i+x][3*j+y] for x in range(3) for y in range(3) if(board[3*i+x][3*j+y] != ".")]
+                
+                if(len(temp) != len(set(temp))):
+                    return False
+        
+        return True
+
+
+
+
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        """
+        Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 3
+        Output: true
+        
+        Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 13
+        Output: false
+        """
+        if(target < matrix[0][0] or matrix[-1][-1] < target):
+            return False
+        
+        row = 0
+        while(row < len(matrix) and target >= matrix[row][0]):
+            row += 1
+            
+        if(target in matrix[row - 1]):
+            return True
+        
+        return False
+
+
+
+
+
+
+
+
 
 
 
