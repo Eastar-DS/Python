11a1796cdabb5dda9477f72ffd6136777897e14d
Eastar-DS <dororo0104@naver.com>
2021-12-16 18:51:45 +0900

    2021_1216

--- Programmers/Hash.py
+++ Programmers/Hash.py
@@ -137,7 +137,16 @@ def solution4(genres, plays):
 
 
 
-
+def solution41(genres, plays):
+    answer = []
+    d = {e:[] for e in set(genres)}
+    for e in zip(genres, plays, range(len(plays))):
+        d[e[0]].append([e[1] , e[2]])
+    genreSort =sorted(list(d.keys()), key= lambda x: sum( map(lambda y: y[0],d[x])), reverse = True)
+    for g in genreSort:
+        temp = [e[1] for e in sorted(d[g],key= lambda x: (x[0], -x[1]), reverse = True)]
+        answer += temp[:min(len(temp),2)]
+    return answer
 
 
 
