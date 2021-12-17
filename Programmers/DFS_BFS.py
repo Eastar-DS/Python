f16145fc28a1b4cf8f9f4e375fc13d75a32f6dd2
Eastar-DS <dororo0104@naver.com>
2021-12-17 23:30:02 +0900

    2021_1217

--- Programmers/DFS_BFS.py
+++ Programmers/DFS_BFS.py
@@ -24,8 +24,28 @@ def solution11(numbers, target):
 
 
 
-
-#today is holiday!
+def solution2(n, computers):
+    answer = 0
+    def network(i,j):
+        if(computers[i][j] != 1):
+            return
+        else:
+            computers[i][j] = -1
+            for k in range(0,n):
+                if(computers[j][k] == 1):
+                    network(j,k)
+            
+            if(computers[i][i] == 1):
+                computers[i][i] = -1
+            if(computers[j][j] == 1):
+                computers[j][j] = -1
+            
+    for i in range(0,n):
+        for j in range(0,n):
+            if(computers[i][j] == 1):
+                network(i,j)
+                answer += 1
+    return answer
 
 
 
