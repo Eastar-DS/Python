93d15e862e6b314e0b409587e416e2c1bd2a0400
Eastar-DS <dororo0104@naver.com>
2021-12-25 20:12:30 +0900

    2021_1225

--- Leetcode/Medium_Problems.py
+++ Leetcode/Medium_Problems.py
@@ -617,15 +617,87 @@ class Solution210:
         return output
 
 
+#56. Merge Intervals
+class Solution56:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        """
+        Input: intervals = [[1,3],[2,6],[8,10],[15,18]]
+        Output: [[1,6],[8,10],[15,18]]
+        
+        Input: intervals = [[1,4],[4,5]]
+        Output: [[1,5]]
+        """
+        intervals.sort(key= lambda x: x[0])
+        
+        i=1
+        while(i<len(intervals)):
+            if(intervals[i][0] <= intervals[i-1][1] and intervals[i][1] >= intervals[i-1][1]):                
+                intervals[i-1] = [intervals[i-1][0], intervals[i][1]]
+                intervals.pop(i)
+            elif(intervals[i][0] <= intervals[i-1][1] and intervals[i][1] < intervals[i-1][1]):
+                intervals.pop(i)
+            else:
+                i += 1
+                
+        return intervals
 
 
+    """
+    intervals [[1, 3], [2, 6], [8, 10], [15, 18]]
+intervals.sort [[1, 3], [2, 6], [8, 10], [15, 18]]
 
+interval = [1,3]
+merged =[]
+not merged:
+	merged =[ [1,3] ]
 
+interval =[2,6]
+merged = [ [1,3] ]
+merged[-1][-1] = 3 > interval[0] = 2:
+	merged[-1][-1] = max(merged[-1][-1] = 3 ,interval[-1] = 6) =6
+merged = [[1,6]]
+    """
 
-
-
-
-
+    def merge1(self, intervals: List[List[int]]) -> List[List[int]]:
+        intervals.sort(key =lambda x: x[0])
+        merged =[]
+        for i in intervals:
+			# if the list of merged intervals is empty 
+			# or if the current interval does not overlap with the previous,
+			# simply append it.
+            if not merged or merged[-1][-1] < i[0]:
+                merged.append(i)
+			# otherwise, there is overlap,
+			#so we merge the current and previous intervals.
+            else:
+                merged[-1][-1] = max(merged[-1][-1], i[-1])
+        return merged
+        """
+        Time complexity:
+            In python, use sort method to a list costs O(nlogn), where n is the length of the list.
+            The for-loop used to merge intervals, costs O(n).
+            O(nlogn)+O(n) = O(nlogn)
+            So the total time complexity is O(nlogn).
+        Space complexity:
+            The algorithm used a merged list and a variable i.
+            In the worst case, the merged list is equal to the length of the input intervals list. 
+            So the space complexity is O(n), where n is the length of the input list.
+        """
+    def merge2(self, intervals: List[List[int]]) -> List[List[int]]:
+        intervals.sort()
+        merged = []
+        for i in range(len(intervals)):
+            if merged == []:
+                merged.append(intervals[i])
+            else:
+                previous_end = merged[-1][1]
+                current_start = intervals[i][0]
+                current_end = intervals[i][1]
+                if previous_end >= current_start: # overlap
+                    merged[-1][1] = max(previous_end,current_end)
+                else:
+                    merged.append(intervals[i])
+        return merged
 
 
 
--- momodeep2/test.py
+++ momodeep2/test.py
@@ -11,129 +11,43 @@ from typing import *
 import numpy as np
 import torch 
 
-#lec1
-m1 = torch.FloatTensor([[1,2],[3,4]])
+import torchvision.datasets as dsets
+import torchvision.transforms as transforms
+import matplotlib.pyplot as plt
+import random
+
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+random.seed(777)
+torch.manual_seed(777)
+if device == 'cuda':
+    torch.cuda.manual_seed_all(777)
+    
+training_epochs = 15    
+batch_size = 100    
+
+mnist_train = dsets.MNIST(root="MNIST_data/", train = True, transform=transforms.ToTensor(), download=True)
+mnist_test = dsets.MNIST(root="MNIST_data/", train = False, transform=transforms.ToTensor(), download=True)
+
+data_loader = torch.utils.data.DataLoader(dataset = mnist_train, batch_size = batch_size, shuffle=True, drop_last=True)
 """
-Broadcasting : 자동적으로 사이즈맞춰서 계산
-[[1,2]] + [3] = [[4,5]]
-[[1,2]] + [[3],[4]] = [[4,5],[5,6]]
-
-*(.mul) vs .matmul
-[[1,2],[3,4]] * [[1],[2]] = [[1,2],[6,8]]
-[[1,2],[3,4]].matmul([[1],[2]]) = [[5],[11]]
-
-.mean()
-m1 = torch.FloatTensor([[1,2],[3,4]])
-m1.mean() = 2.5
-m1.mean(dim=0) = [2,3]
-m1.mean(dim=1) = [1.5,3.5]
-m1.mean(dim=-1) = [1.5,3.5]
-
-.sum()
-m1.sum() = 10
-m1.sum(dim=0) = 10
-m1.sum(dim=1) = [3,7]
-m1.sum(dim=-1) = [3,7]
-
-.max()
-m1.max() = 4
-m1.max(dim=0) = [3,4], [1,1](argmax)
-m1.max(dim=0)[0] = [3,4]
-m1.max(dim=0)[1] = [1,1]
-m1.max(dim=1) = [2,4],[1,1]
-m1.max(dim=-1) = [2,4],[1,1]
+DataLoader : 어떤데이터를 로드할거냐
+batch_size : 데이터를 몇개씩 잘라서 불러올래
+shuffle : 순서를 섞어서 불러올래
+drop_last : 마지막 어케할거야
 """
-#lec2
-t = np.array([[[0,1,2],
-              [3,4,5]],
-              [[6,7,8],
-               [9,10,11]]])
-ft = torch.FlatTensor(t)
+linear = torch.nn.Linear(784,10,bias=True).to(device)
 """
-.view() : ==Reshape
-ft.view([-1,3]) = [[012],[345],[678],[9,10,11]]
-ft.view([-1,3]).shape = [4,3]
-ft.view([-1,1,3]) = [[[012]]
-                     [[345]]
-                     [[678]]
-                     [[9,10,11]]] : 4개의 batch, batch당1개의데이터, 3개의특성
-ft.view([-1,1,3]).shape = [4,1,3]
-
+torch.nn 패키지에있는 리니어레이어하나를 사용할거다. 
+인풋784 아웃풋10
 """
-
-ft = torch.FloatTensor([[0],[1],[2]])
+criterion = torch.nn.CrossEntropyLoss().to(device)
+optimizer = torch.optim.SGD(linear.parameters(), lr=0.1)
 """
-.squeeze() : 사이즈 1 쥐어짜서 없애줌.
-ft.shape = [3,1]
-ft.squeeze(dim = 0) = 그대로 => why? 첫번째 사이즈가 1이면 쥐어짜는거임.
-ft.squeeze() = [0,1,2]
-ft.shape = [3]
+소프트맥스 클래시파이어에 대해서 크로스엔트로피코스트를 사용할거고, 
+파이토치에서는 크로스엔트로피로스가 소프트맥스를 자동으로 계산하기때문에 따로선언하지않음.
+
+linear.parameters() = W,b
 """
-
-ft = torch.FloatTensor([0,1,2])
-"""
-.unsqueeze()
-ft.shape = [3]
-ft.unsqueeze(0) = ft.unsqueeze(dim = 0) = [[0,1,2]]
-ft.unsqueeze(0).shape = [1,3]
-
-ft.view(1,-1) = [[0,1,2]]
-
-ft.unsqueeze(1) = [[0][1][2]]
-"""
-
-lt = torch.LongTensor([1,2,3,4])
-bt = torch.ByteTensor([True,False,False,True])
-"""
-Type Casting
-lt.float()
-bt.long() = [1,0,0,1]
-bt.float() = [1.,0.,0.,1.]
-"""
-
-x = torch.FloatTensor([[1,2],[3,4]])
-y = torch.FloatTensor([[5,6],[7,8]])
-"""
-Concatenate
-.cat()
-torch.cat([x,y], dim = 0) = 위아래로 잇는다. (2,4)
-torch.cat([x,y], dim = 1) = 옆으로 잇는다. (4,2)
-"""
-
-x = torch.FloatTensor([1,4])
-y = torch.FloatTensor([2,5])
-z = torch.FloatTensor([3,6])
-"""
-Stacking
-.stack()
-torch.stack([x,y,z]) = [[1,4],[2,5],[3,6]]
-torch.stack([x,y,z], dim = 1) = [[1,2,3],[4,5,6]]
-
-torch.cat([x.unsqueeze(0),y.unsqueeze(0),z.unsqueeze(0)], dim=0)
-"""
-
-x = torch.FloatTensor([[0,1,2],[2,1,0]])
-"""
-Ones and Zeros
-torch.ones_like(x) = [[1,1,1],[1,1,1]]
-torch.zeros_like(x) = [[0,0,0],[0,0,0]]
-
-중요한것은 이것들의 device가 같은곳에서 생김. 어떤 연산은 같은 디바이스에 있어야 가능하다!
-"""
-
-x = torch.FloatTensor([[1,2],[3,4]])
-"""
-x.mul(2.) = [[2,4],[6,8]]
-x는 변하지않음.
-x.mul_(2.) = [[2,4],[6,8]]
-x = [[2,4],[6,8]]
-x가 변함.
-"""
-
-
-
-
-
 
 
 
