ab05fa660e4baa9b322e2a61238ddd8afae20289
Eastar-DS <dororo0104@naver.com>
2022-01-12 10:33:27 +0900

    Medium_701

--- Leetcode/Medium_Problems.py
+++ Leetcode/Medium_Problems.py
@@ -1020,8 +1020,13 @@ class Solution1041:
         return False
             
         
-        
-
+#701. Insert into a Binary Search Tree
+class Solution701:
+    def insertIntoBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+        if(root == None): return TreeNode(val);
+        if(root.val < val): root.right = self.insertIntoBST(root.right, val);
+        else: root.left = self.insertIntoBST(root.left, val);
+        return(root)
         
 
 
